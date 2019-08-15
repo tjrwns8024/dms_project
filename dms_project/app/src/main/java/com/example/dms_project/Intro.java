0508c7e0a0a47c1b34e07c4f11c03ff88ab31b8e
seokjun <tjrwns8024@naver.com>
2019-08-15 23:38:10 +0900

    example

---
+++ dms_project/app/src/main/java/com/example/dms_project/Intro.java
@@ -0,0 +1,26 @@
+package com.example.dms_project;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Intro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Intro.this, MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                finish();
+            }
+        }, 3000);
+    }
+}
